d1cbebf8515a172a953cca104593ba645e36fe8e
schlenkerc18 <schlenkerc18@hanover.edu>
2022-12-02 21:07:32 -0500

    creating new GradeBookTypes

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,23 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name) // this makes standardgradebook execute basegradebook constructor before executing its own
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
